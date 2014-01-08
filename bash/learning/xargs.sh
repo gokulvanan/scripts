91d2ec3ef4d884f7d185d4c30ad918ba8f258b5c
Gokulvanan <gokulvanan@gmail.com>
2014-01-08 10:42:59 +0530

    added new command usefull in backing uncommited changes during git pull

--- bash/learning/xargs.sh
+++ bash/learning/xargs.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 find /etc -name '*.sh' | xargs head -2
+
+#command to move modified files to a backup dir
+git status | grep modified | awk '{ print $3 }'| xargs -t -I '{}' cp '{}' ../temp_bk/
+# note -t prints each command and -I is a delim to identify first arg of cp as output from awk with line delim
